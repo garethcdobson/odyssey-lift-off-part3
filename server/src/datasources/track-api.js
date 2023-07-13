de28cc343b9f984524511a30c40cb8561ad6471b
Gareth Dobson <gareth.dobson@acresoftware.com>
2023-07-13 11:09:17 +0100

    Adding new endpoints for querying single tracks and modules, updating schema and resolver

--- server/src/datasources/track-api.js
+++ server/src/datasources/track-api.js
@@ -1,11 +1,19 @@
-const { RESTDataSource } = require('@apollo/datasource-rest');
+const { RESTDataSource } = require("@apollo/datasource-rest");
 
 class TrackAPI extends RESTDataSource {
   // the Catstronauts catalog is hosted on this server
-  baseURL = 'https://odyssey-lift-off-rest-api.herokuapp.com/';
+  baseURL = "https://odyssey-lift-off-rest-api.herokuapp.com/";
 
   getTracksForHome() {
-    return this.get('tracks');
+    return this.get("tracks");
+  }
+
+  getTrack(trackId) {
+    return this.get(`track/${trackId}`);
+  }
+
+  getTrackModules(trackId) {
+    return this.get(`track/${trackId}/modules`);
   }
 
   getAuthor(authorId) {
